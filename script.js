164d4e5370b702dc26baad3d9a2ec9e1e21a539b
Ушаков Игорь <ushakov@dcp24.tech>
2023-08-01 14:53:17 +0300

    keydown listener added

--- script.js
+++ script.js
@@ -9,14 +9,27 @@
 
 	const popup = document.createElement('div')
 	popup.setAttribute("id", "internalurlpopup");
-	popup.innerHTML = `<div>В статье найдено внутренних ссылок: ${result.length} <br/> ${result.map(item => `—————————— <br/> ${item[0]} <br/> ${item[1]}`)}</div>`;
-	popup.setAttribute("style", "display:flex;justify-content:space-between;position: fixed;top: 0;background: white;z-index: 100000;width: calc(100% - 48px);padding: 24px;box-shadow: 0 5px 5px rgba(182, 182, 182, 0.75);");
+	popup.innerHTML = `<div style="overflow-y: scroll; width: 100%">В статье найдено внутренних ссылок: <b>${result.length}</b> <br/> ${result.map(item => `—————————— <br/> <b>${item[0]}</b> <br/><br/> <a href=${item[1]}">${item[1]}</a>`).join('<br>')}</div>`;
+	popup.setAttribute("style", "display:flex;justify-content:space-between;max-height: 500px;overflow-y: hidden;position: fixed;top: 0;background: white;z-index: 100000;width: calc(100% - 48px);padding: 24px;box-shadow: 0 5px 5px rgba(182, 182, 182, 0.75);");
 
 	const closeButton = document.createElement('div');
 	closeButton.innerHTML = '×'
 	closeButton.setAttribute("style", "font-size: 24px;cursor:pointer");
 	closeButton.setAttribute("onclick", "document.getElementById('internalurlpopup').remove()");
 	
+	function removeEvent() {
+		document.removeEventListener('keydown', removePopup); 
+	}
+	
+	function removePopup(event) {
+		if (event.key === 'Escape') {
+			document.getElementById('internalurlpopup').remove()
+			removeEvent();
+		}
+	}
+	
+	document.addEventListener('keydown', removePopup);
+
 	popup.appendChild(closeButton);
 
 	document.getElementsByTagName('body')[0].appendChild(popup);
