c63c044c42052acb05f94caafa1e869e8fe3aa87
Ушаков Игорь <ushakov@dcp24.tech>
2023-08-01 14:37:43 +0300

    done

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 	const popup = document.createElement('div')
 	popup.setAttribute("id", "internalurlpopup");
-	popup.innerHTML = `<div>В статье найдено внутренних ссылок: ${result.length} <br/> ${result.map(item => `————————— <br/> ${item[0]} <br/> ${item[1]}`)}</div>`;
+	popup.innerHTML = `<div>В статье найдено внутренних ссылок: ${result.length} <br/> ${result.map(item => `—————————— <br/> ${item[0]} <br/> ${item[1]}`)}</div>`;
 	popup.setAttribute("style", "display:flex;justify-content:space-between;position: fixed;top: 0;background: white;z-index: 100000;width: calc(100% - 48px);padding: 24px;box-shadow: 0 5px 5px rgba(182, 182, 182, 0.75);");
 
 	const closeButton = document.createElement('div');
