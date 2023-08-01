7c25b3be5ae878ccad30b3e55eb053218f4dcd21
Ушаков Игорь <ushakov@dcp24.tech>
2023-08-01 13:27:52 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+(() => {
+	const links = document.querySelector(content).querySelectorAll('a');
+	let result = []
+	links.forEach(item => {
+		if (item.href && (item.href[0] === '/' || item.href.includes(url))) {
+			result.push([item.text, item.href]);
+		}
+	})
+
+	const popup = document.createElement('div')
+	popup.setAttribute("id", "internalurlpopup");
+	popup.innerHTML = `<div>В статье найдено внутренних ссылок: ${result.length} <br/> ${result.map(item => `————————— <br/> ${item[0]} <br/> ${item[1]}`)}</div>`;
+	popup.setAttribute("style", "display:flex;justify-content:space-between;position: fixed;top: 0;background: white;z-index: 100000;width: calc(100% - 48px);padding: 24px;box-shadow: 0 5px 5px rgba(182, 182, 182, 0.75);");
+
+	const closeButton = document.createElement('div');
+	closeButton.innerHTML = '×'
+	closeButton.setAttribute("style", "font-size: 24px;cursor:pointer");
+	closeButton.setAttribute("onclick", "document.getElementById('internalurlpopup').remove()");
+	
+	popup.appendChild(closeButton);
+
+	document.getElementsByTagName('body')[0].appendChild(popup);
+})();
